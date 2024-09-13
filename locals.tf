07e7683ff2eb419dacb4f57ed40a008390e74f03
Jared Holgate <jaredholgate@microsoft.com>
2024-09-13 11:31:47 +0100

    precedence

--- locals.tf
+++ locals.tf
@@ -3,5 +3,5 @@ locals {
   inherited_tags_subscription        = var.tag_inheritance == null ? {} : (var.tag_inheritance.subscription ? data.azurerm_subscription.tags[0].tags : {})
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
   subscription_id                    = coalesce(var.subscription_id, data.azurerm_client_config.this.subscription_id)
-  tags                               = merge(local.inherited_tags_resource_group, local.inherited_tags_subscription, var.tags)
+  tags                               = merge(local.inherited_tags_subscription, local.inherited_tags_resource_group, var.tags)
 }
